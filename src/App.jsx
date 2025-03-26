ee925598b9e0ade127b30d10eaf3f76588a44d6a
animeshVG <animesh.raikwar@vinzglobal.com>
2025-03-26 17:22:19 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,75 @@
-import React from 'react'
+import React from "react";
+import Image from "./assets/image20.png";
+import QR from "./assets/MainQR.png";
 
 const App = () => {
   return (
     <div
-    style={{height:"100vh",width:"100%" ,backgroundColor:'red', display:"flex",justifyContent:"center",alignItems:"center"}}
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <div style={{height:"50%",width:"70%",backgroundColor:"blue"}}></div>
+      <div
+        style={{
+          height: "50%",
+          width: "70%",
+          backgroundColor: "#266788",
+          borderRadius: "10px",
+        }}
+      >
+        <div
+          style={{
+            height: "20%",
+            width: "100%",
+            // backgroundColor: "green",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={Image} alt="" />
+        </div>
+        <div
+          style={{
+            height: "80%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              height: "80%",
+              width: "50%",
+              backgroundColor: "white",
+              // marginTop: -40,
+              borderRadius: "5%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={QR} style={{ height: "200px", width: "200px" }} />
+          </div>
+          <p
+            style={{
+              fontSize: "20px",
+              color: "white",
+              fontFamily: "sans-serif",
+            }}
+          >
+            To Download the App Scan this QR Code
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
