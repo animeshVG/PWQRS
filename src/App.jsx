24d3019684a3bc5b0aa6e687e4db06a1a6dbfda0
animeshVG <animesh.raikwar@vinzglobal.com>
2025-03-26 13:15:57 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div
+    style={{height:"100vh",width:"100%" ,backgroundColor:'red', display:"flex",justifyContent:"center",alignItems:"center"}}
+    >
+      <div style={{height:"50%",width:"70%",backgroundColor:"blue"}}></div>
+    </div>
+  )
+}
+
+export default App
